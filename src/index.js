0cc529fd024bd8b26a57465fb70e1b03072e8825
Yash Gupta <yashvinodgupta6@gmail.com>
2025-07-19 20:11:31 +0530

    connected to atlas mongoDB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-import mongoose from "mongoose";
+// require("dotenv").config({path: "./.env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-function connectDB(){
-
-}
+dotenv.config({ path: "./.env" });
 
 connectDB();
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERRR : ", error);
+      throw error;
+    });
+
+    app.listen(process.env.port, () => {
+      console.log(`Server is running on port ${process.env.port}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();
+*/
